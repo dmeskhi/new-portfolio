f00598d9a84c766a2d383e16ea7fb3ca637637d7
David Meskhishvili <dmeskh@gmail.com>
2024-08-04 23:37:44 -0700

    Make web contact form work

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 const menuIcon = document.querySelector(".menu-icon");
 const container = document.querySelector(".container");
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
 
 menuIcon.addEventListener("click", () => {
   container.classList.toggle("change");
+});
+
+form.addEventListener('submit', function(e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  result.innerHTML = "Please wait..."
+
+  fetch('https://api.web3forms.com/submit', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          },
+          body: json
+      })
+      .then(async (response) => {
+          let json = await response.json();
+          if (response.status == 200) {
+              result.innerHTML = json.message;
+          } else {
+              console.log(response);
+              result.innerHTML = json.message;
+          }
+      })
+      .catch(error => {
+          console.log(error);
+          result.innerHTML = "Something went wrong!";
+      })
+      .then(function() {
+          form.reset();
+          setTimeout(() => {
+              result.style.display = "none";
+          }, 3000);
+      });
 });
